9399833d586de62b886a2234a87156ab160f3c7d
arcstep <43801@qq.com>
2022-10-20 12:45:50 +0800

    生成任务配置项时，保存带类型前缀的任务参数

--- R/data-plyr.R
+++ R/data-plyr.R
@@ -5,14 +5,14 @@
 #' @export
 dp_read <- function(s_dsName, s_dataName = "@result") {
   ex <- expression({
-    ds_read0(dsName, cacheTopic)
+    ds_read0(s_dsName, cacheTopic)
   })
   list(
     "scriptType" = "dp_read",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "dsName" = s_dsName)
+      "s_dataName" = s_dataName,
+      "s_dsName" = s_dsName)
     )
 }
 
@@ -21,13 +21,13 @@ dp_read <- function(s_dsName, s_dataName = "@result") {
 #' @export
 dp_collect <- function(s_dataName = "@result") {
   ex <- expression({
-    get(dataName) |> collect()
+    get(s_dataName) |> collect()
   })
   list(
     "scriptType" = "dp_collect",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName)
+      "s_dataName" = s_dataName)
   )
 }
 
@@ -39,7 +39,7 @@ dp_collect <- function(s_dataName = "@result") {
 #' 
 #' 允许为数据集增加多个阈值查询条件，缩小筛查范围。
 #' 
-#' taskId、column、op、value等参数构造唯一的dp_filter表达式，
+#' taskId、s_column、op_name、fn_value等参数构造唯一的dp_filter表达式，
 #' 这将允许从UI生成或还原该操作。
 #' 
 #' 只要包括以下逻辑判断：
@@ -52,10 +52,10 @@ dp_collect <- function(s_dataName = "@result") {
 #' }
 #' 
 #' @param taskId 任务ID
-#' @param column 列名
-#' @param op 阈值范围判断符号
-#' @param value 阈值
-#' @param dataName 内存中的数据框名称，默认为 @result
+#' @param s_column 列名
+#' @param op_name 阈值范围判断符号
+#' @param fn_value 阈值
+#' @param s_dataName 内存中的数据框名称，默认为 @result
 #' @param taskTopic 任务定义的主题文件夹
 #' @family data-plyr function
 #' @export
@@ -67,28 +67,28 @@ dp_filter <- function(s_column, op_name, fn_value = list(NULL), s_dataName = "@r
   ## 创建任务表达式
   if(op_name %in% c(">", "<", ">=", "<=", "==", "!=", "%in%")) {
     ex <- expression({
-      get(dataName) |>
-        filter(do.call(!!sym(op), args = list(!!sym(column), unlist(value)))) |>
+      get(s_dataName) |>
+        filter(do.call(!!sym(op_name), args = list(!!sym(s_column), unlist(fn_value)))) |>
         collect()
     })
   } else if(stringr::str_detect(op_name, "^[@#% ]*time[@#% ]+(>|<|>=|<=|==)[ ]*$")) {
     ex <- expression({
-      myop <- stringr::str_replace(op, "[@#%]?time[@#% ]+", "") |> stringr::str_trim()
-      data <- get(dataName)
+      myop <- stringr::str_replace(op_name, "[@#%]?time[@#% ]+", "") |> stringr::str_trim()
+      data <- get(s_dataName)
       param <- list()
-      x1 <- data[[column]] |> as_datetime(tz = "Asia/Shanghai")
-      x2 <- unlist(value) |> as_datetime(tz = "Asia/Shanghai")
+      x1 <- data[[s_column]] |> as_datetime(tz = "Asia/Shanghai")
+      x2 <- unlist(fn_value) |> as_datetime(tz = "Asia/Shanghai")
       data |>
         filter(do.call(myop, args = list(x1, x2))) |>
         collect()
     })
   } else if(stringr::str_detect(op_name, "^[@#% ]*date[@#% ]+(>|<|>=|<=|==)[ ]*$")) {
     ex <- expression({
-      myop <- stringr::str_replace(op, "[@#%]?date[@#% ]+", "") |> stringr::str_trim()
-      data <- get(dataName)
+      myop <- stringr::str_replace(op_name, "[@#%]?date[@#% ]+", "") |> stringr::str_trim()
+      data <- get(s_dataName)
       param <- list()
-      x1 <- data[[column]] |> as_date()
-      x2 <- unlist(value) |> as_date()
+      x1 <- data[[s_column]] |> as_date()
+      x2 <- unlist(fn_value) |> as_date()
       data |>
         filter(do.call(myop, args = list(x1, x2))) |>
         collect()
@@ -96,14 +96,14 @@ dp_filter <- function(s_column, op_name, fn_value = list(NULL), s_dataName = "@r
   } else if(op_name %in% c("%nin%")) {
     ## 将 %nin% 转换为可以惰性执行的 %in%
     ex <- expression({
-      get(dataName) |>
-        filter(!do.call("%in%", args = list(!!sym(column), unlist(value)))) |>
+      get(s_dataName) |>
+        filter(!do.call("%in%", args = list(!!sym(s_column), unlist(fn_value)))) |>
         collect()
     })
   } else if(op_name %in% c("%regex%", "%not-regex%")) {
     ## 正则表达式需要不能惰性执行，需要提前collect数据
     ex <- expression({
-      get(dataName) |> collect() |> filter(do.call(!!sym(op), args = list(!!sym(column), unlist(value))))
+      get(s_dataName) |> collect() |> filter(do.call(!!sym(op_name), args = list(!!sym(s_column), unlist(fn_value))))
     })
   } else {
     stop("<dp_filter> Unknown OP: ", op_name)
@@ -112,10 +112,10 @@ dp_filter <- function(s_column, op_name, fn_value = list(NULL), s_dataName = "@r
     "scriptType" = "dp_filter",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "column" = s_column,
-      "op" = op_name,
-      "value" = fn_value))
+      "s_dataName" = s_dataName,
+      "s_column" = s_column,
+      "op_name" = op_name,
+      "fn_value" = fn_value))
 }
 
 #' @title 头部数据
@@ -123,14 +123,14 @@ dp_filter <- function(s_column, op_name, fn_value = list(NULL), s_dataName = "@r
 #' @export
 dp_head <- function(i_n = 10, s_dataName = "@result") {
   ex <- expression({
-    get(dataName) |> head(n)
+    get(s_dataName) |> head(i_n)
   })
   list(
     "scriptType" = "dp_head",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "n" = i_n)
+      "s_dataName" = s_dataName,
+      "i_n" = i_n)
   )
 }
 
@@ -139,14 +139,14 @@ dp_head <- function(i_n = 10, s_dataName = "@result") {
 #' @export
 dp_tail <- function(i_n = 10, s_dataName = "@result") {
   ex <- expression({
-    get(dataName) |> tail(n)
+    get(s_dataName) |> tail(i_n)
   })
   list(
     "scriptType" = "dp_tail",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "n" = i_n)
+      "s_dataName" = s_dataName,
+      "i_n" = i_n)
   )
 }
 
@@ -155,17 +155,17 @@ dp_tail <- function(i_n = 10, s_dataName = "@result") {
 #' @export
 dp_n_max <- function(s_orderColumn, i_n = 10, b_with_ties = FALSE, s_dataName = "@result") {
   ex <- expression({
-    mydata <- get(dataName) |> collect()
-    mydata |> slice_max(order_by = mydata[[orderColumn]], n = n, with_ties = with_ties)
+    mydata <- get(s_dataName) |> collect()
+    mydata |> slice_max(order_by = mydata[[s_orderColumn]], n = i_n, with_ties = b_with_ties)
   })
   list(
     "scriptType" = "dp_n_max",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "orderColumn" = s_orderColumn,
-      "n" = i_n,
-      "with_ties" = b_with_ties)
+      "s_dataName" = s_dataName,
+      "s_orderColumn" = s_orderColumn,
+      "i_n" = i_n,
+      "b_with_ties" = b_with_ties)
   )
 }
 
@@ -174,17 +174,17 @@ dp_n_max <- function(s_orderColumn, i_n = 10, b_with_ties = FALSE, s_dataName = 
 #' @export
 dp_n_min <- function(s_orderColumn, i_n = 10, b_with_ties = FALSE, s_dataName = "@result") {
   ex <- expression({
-    mydata <- get(dataName) |> collect()
-    mydata |> slice_min(order_by = mydata[[orderColumn]], n = n, with_ties = with_ties)
+    mydata <- get(s_dataName) |> collect()
+    mydata |> slice_min(order_by = mydata[[s_orderColumn]], n = i_n, with_ties = b_with_ties)
   })
   list(
     "scriptType" = "dp_n_min",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "orderColumn" = s_orderColumn,
-      "n" = i_n,
-      "with_ties" = b_with_ties)
+      "s_dataName" = s_dataName,
+      "s_orderColumn" = s_orderColumn,
+      "i_n" = i_n,
+      "b_with_ties" = b_with_ties)
   )
 }
 
@@ -196,21 +196,21 @@ dp_n_min <- function(s_orderColumn, i_n = 10, b_with_ties = FALSE, s_dataName = 
 dp_arrange <- function(sn_columns = list(),
                        b_desc = FALSE, b_by_group = FALSE, s_dataName = "@result") {
   ex <- expression({
-    mydata <- get(dataName)
-    if(desc) {
-      mydata |> arrange(desc(!!!syms(columns)), .by_group = by_group)
+    mydata <- get(s_dataName)
+    if(b_desc) {
+      mydata |> arrange(desc(!!!syms(sn_columns)), .by_group = b_by_group)
     } else {
-      mydata |> arrange(!!!syms(columns), .by_group = by_group)
+      mydata |> arrange(!!!syms(sn_columns), .by_group = b_by_group)
     }
   })
   list(
     "scriptType" = "dp_n_min",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "columns" = sn_columns,
-      "desc" = b_desc,
-      "by_group" = b_by_group)
+      "s_dataName" = s_dataName,
+      "sn_columns" = sn_columns,
+      "b_desc" = b_desc,
+      "b_by_group" = b_by_group)
   )
 }
 
@@ -222,21 +222,21 @@ dp_arrange <- function(sn_columns = list(),
 dp_select <- function(sn_columns = list(), b_everything = FALSE,
                       s_regex = NULL, s_dataName = "@result") {
   ex <- expression({
-    mydata <- get(dataName)
-    if(everything) {
-      mydata |> select(contains(columns), matches(regex), everything())
+    mydata <- get(s_dataName)
+    if(b_everything) {
+      mydata |> select(contains(sn_columns), matches(s_regex), everything())
     } else {
-      mydata |> select(contains(columns), matches(regex))
+      mydata |> select(contains(sn_columns), matches(s_regex))
     }
   })
   list(
     "scriptType" = "dp_n_min",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "columns" = sn_columns |> unlist(),
-      "regex" = s_regex %empty% "^mamahannihuijiachifan$",
-      "everything" = b_everything)
+      "s_dataName" = s_dataName,
+      "sn_columns" = sn_columns |> unlist(),
+      "s_regex" = s_regex %empty% "^mamahannihuijiachifan$",
+      "b_everything" = b_everything)
   )
 }
 
@@ -245,14 +245,14 @@ dp_select <- function(sn_columns = list(), b_everything = FALSE,
 #' @export
 dp_rename <- function(s_newName, s_oldName, s_dataName = "@result") {
   ex <- expression({
-    get(dataName) |> rename({{newName}} := oldName)
+    get(s_dataName) |> rename({{s_newName}} := s_oldName)
   })
   list(
     "scriptType" = "dp_n_min",
     "taskScript" = ex |> as.character(),
     "params" = list(
-      "dataName" = s_dataName,
-      "newName" = s_newName,
-      "oldName" = s_oldName)
+      "s_dataName" = s_dataName,
+      "s_newName" = s_newName,
+      "s_oldName" = s_oldName)
   )
 }
