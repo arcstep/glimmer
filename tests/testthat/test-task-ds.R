8fe03a027ca1a908fdf86d964ce9a379cf42becc
arcstep <43801@qq.com>
2022-10-26 16:43:08 +0800

    Fix: test-task-ds, 在任务调度中使用ds函数

--- tests/testthat/test-task-ds.R
+++ tests/testthat/test-task-ds.R
@@ -19,7 +19,7 @@ test_that("<ds_collect>", {
   temp_remove()
 })
 
-test_that("<gali_write>", {
+test_that("<ds_write>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
@@ -27,14 +27,14 @@ test_that("<gali_write>", {
   ds_init("车数据", keyColumns = "rowname", data = m |> head())
   
   task_create("cars/ds_read0") |>
-    task_func_add("gali_write", params = list(e_dsName = "车数据")) |>
+    task_func_add("ds_write", params = list(dsName = "车数据")) |>
     task_run(`@ds` = m |> slice(1:10))
   ds_read0("车数据") |> collect() |> nrow() |>
     testthat::expect_equal(10)
   
   task_create("cars/ds_read02") |>
     task_empty_add(params = list(`@ds` = m |> slice(5:15))) |>
-    task_func_add("gali_write", params = list(e_dsName = "车数据")) |>
+    task_func_add("ds_write", params = list(dsName = "车数据")) |>
     task_run()
   ds_read0("车数据") |> collect() |> nrow() |>
     testthat::expect_equal(15)
@@ -42,40 +42,40 @@ test_that("<gali_write>", {
   temp_remove()
 })
 
-test_that("<gali_ds_filter>：一般流程", {
+test_that("<ds_filter>：一般流程", {
   sample_init()
   
   ## 使用默认的 @result 获得返回值
-  mtcars |> gali_ds_filter("cyl", ">", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", ">", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl > 6) |> nrow())
   
   ## 使用新的变量名
-  mtcars |> gali_ds_filter("cyl", ">", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", ">", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl > 6) |> nrow())
   
   temp_remove()
 })
 
-test_that("<gali_ds_filter>：比较操作符", {
-  mtcars |> gali_ds_filter("cyl", "<", 6) |> nrow() |>
+test_that("<ds_filter>：比较操作符", {
+  mtcars |> ds_filter("cyl", "<", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl < 6) |> nrow())
   
-  mtcars |> gali_ds_filter("cyl", ">=", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", ">=", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl >= 6) |> nrow())
   
-  mtcars |> gali_ds_filter("cyl", "<=", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", "<=", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl <= 6) |> nrow())
   
-  mtcars |> gali_ds_filter("cyl", "==", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", "==", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl == 6) |> nrow())
   
-  mtcars |> gali_ds_filter("cyl", "!=", 6) |> nrow() |>
+  mtcars |> ds_filter("cyl", "!=", 6) |> nrow() |>
     testthat::expect_equal(mtcars |> filter(cyl != 6) |> nrow())
 })
 
-test_that("<gali_ds_filter>：正则表达式", {
+test_that("<ds_filter>：正则表达式", {
   mtcars |> rownames_to_column() |>
-    gali_ds_filter("rowname", "%regex%", "Mazda") |>
+    ds_filter("rowname", "%regex%", "Mazda") |>
     nrow() |>
     testthat::expect_equal(
       mtcars |>
@@ -84,7 +84,7 @@ test_that("<gali_ds_filter>：正则表达式", {
         nrow())
   
   mtcars |> rownames_to_column() |>
-    gali_ds_filter("rowname", "%not-regex%", "Mazda") |>
+    ds_filter("rowname", "%not-regex%", "Mazda") |>
     nrow() |>
     testthat::expect_equal(
       mtcars |>
@@ -93,9 +93,9 @@ test_that("<gali_ds_filter>：正则表达式", {
         nrow())
 })
 
-test_that("<gali_ds_filter>：包含", {
+test_that("<ds_filter>：包含", {
   mtcars |> rownames_to_column() |>
-    gali_ds_filter("rowname", "%in%", c("Honda Civic", "Fiat 128")) |>
+    ds_filter("rowname", "%in%", c("Honda Civic", "Fiat 128")) |>
     nrow() |>
     testthat::expect_equal(
       mtcars |>
@@ -104,7 +104,7 @@ test_that("<gali_ds_filter>：包含", {
         nrow())
   
   mtcars |> rownames_to_column() |>
-    gali_ds_filter("rowname", "%nin%", c("Honda Civic", "Fiat 128")) |>
+    ds_filter("rowname", "%nin%", c("Honda Civic", "Fiat 128")) |>
     nrow() |>
     testthat::expect_equal(
       mtcars |>
@@ -114,7 +114,7 @@ test_that("<gali_ds_filter>：包含", {
   
 })
 
-test_that("<gali_ds_filter>：比较时间和日期", {
+test_that("<ds_filter>：比较时间和日期", {
   d <- tibble(
     n = 1:5,
     day = c("2020-05-01", "2020-06-01", "2020-05-11", "2020-07-3", "2019-12-30") |> lubridate::as_date(),
@@ -122,19 +122,19 @@ test_that("<gali_ds_filter>：比较时间和日期", {
   )
   
   ## 测试日期格式比较
-  d |> gali_ds_filter("day", "date# >", "2020-05-30") |> nrow() |>
+  d |> ds_filter("day", "date# >", "2020-05-30") |> nrow() |>
     testthat::expect_equal(
       d |> filter(day > as_date("2020-05-30")) |> nrow()
     )
   
   ## 测试时间格式比较
-  d |> gali_ds_filter("dt", "time# >", "2020-05-30") |> nrow() |>
+  d |> ds_filter("dt", "time# >", "2020-05-30") |> nrow() |>
     testthat::expect_equal(
       d |> filter(dt > as_datetime("2020-05-30")) |> nrow()
     )
 })
 
-test_that("<gali_ds_head/ gali_ds_tail>", {
+test_that("<ds_head/ ds_tail>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
@@ -144,24 +144,24 @@ test_that("<gali_ds_head/ gali_ds_tail>", {
   m |> as_tibble() |> ds_write("车数据")
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_head") |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_head") |>
     task_func_add("ds_collect") |>
     task_run() |>
     nrow() |>
     testthat::expect_equal(10)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_head", list(i_n = 5)) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_head", list(n = 5)) |>
     task_func_add("ds_collect") |>
     task_run() |>
     nrow() |>
     testthat::expect_equal(5)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_tail", list(i_n = 6)) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_tail", list(n = 6)) |>
     task_func_add("ds_collect") |>
     task_run() |>
     nrow() |>
@@ -170,7 +170,7 @@ test_that("<gali_ds_head/ gali_ds_tail>", {
   temp_remove()
 })
 
-test_that("<gali_ds_n_max/ gali_ds_n_min>", {
+test_that("<ds_n_max/ ds_n_min>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
@@ -180,15 +180,15 @@ test_that("<gali_ds_n_max/ gali_ds_n_min>", {
   m |> as_tibble() |> ds_write("车数据")
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_n_max", list(s_orderColumn = "cyl", i_n = 3)) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_n_max", list(orderColumn = "cyl", n = 3)) |>
     task_run() |>
     nrow() |>
     testthat::expect_equal(3)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_n_min", list(s_orderColumn = "disp", i_n = 3)) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_n_min", list(orderColumn = "disp", n = 3)) |>
     task_run() |>
     nrow() |>
     testthat::expect_equal(3)
@@ -196,7 +196,7 @@ test_that("<gali_ds_n_max/ gali_ds_n_min>", {
   temp_remove()
 })
 
-test_that("<gali_ds_select>", {
+test_that("<ds_select>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
@@ -206,32 +206,32 @@ test_that("<gali_ds_select>", {
   m |> as_tibble() |> ds_write("车数据")
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_select", list(sv_columns = "cyl")) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_select", list(columns = "cyl")) |>
     task_func_add("ds_collect") |>
     task_run() |>
     ncol() |>
     testthat::expect_equal(1)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_select", list(sv_columns = c("cyl", "disp"))) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_select", list(columns = c("cyl", "disp"))) |>
     task_func_add("ds_collect") |>
     task_run() |>
     ncol() |>
     testthat::expect_equal(2)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_select", list(sv_columns = c("cyl", "disp"), b_everything = T)) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_select", list(columns = c("cyl", "disp"), showOthers = T)) |>
     task_func_add("ds_collect") |>
     task_run() |>
     ncol() |>
     testthat::expect_equal(17)
   
   task_create("cars/ds_read0") |>
-    task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-    task_func_add("gali_ds_select", list(sv_columns = c("cyl", "disp"), s_regex = "^@")) |>
+    task_func_add("ds_read0", list(dsName = "车数据")) |>
+    task_func_add("ds_select", list(columns = c("cyl", "disp"), regex = "^@")) |>
     task_func_add("ds_collect") |>
     task_run() |>
     ncol() |>
@@ -241,7 +241,7 @@ test_that("<gali_ds_select>", {
 })
 
 
-test_that("<gali_arrange>", {
+test_that("<ds_arrange>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
@@ -251,16 +251,16 @@ test_that("<gali_arrange>", {
   m |> as_tibble() |> ds_write("车数据")
   
   (task_create("cars/ds_read0") |>
-      task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-      task_func_add("gali_ds_arrange", list(sv_columns = "disp")) |>
+      task_func_add("ds_read0", list(dsName = "车数据")) |>
+      task_func_add("ds_arrange", list(columns = "disp")) |>
       task_func_add("ds_collect") |>
       task_run() |>
       head(1))$disp |>
     testthat::expect_equal(min(mtcars$disp))
   
   (task_create("cars/ds_read0") |>
-      task_func_add("ds_read0", list(e_dsName = "车数据")) |>
-      task_func_add("gali_ds_arrange", list(sv_columns = "disp", b_desc = T)) |>
+      task_func_add("ds_read0", list(dsName = "车数据")) |>
+      task_func_add("ds_arrange", list(columns = "disp", desc = T)) |>
       task_func_add("ds_collect") |>
       task_run() |>
       head(1))$disp |>
@@ -269,7 +269,7 @@ test_that("<gali_arrange>", {
   temp_remove()
 })
 
-test_that("<gali_rename>", {
+test_that("<ds_rename>", {
   sample_init()
   
   m <- mtcars |> as_tibble() |> rownames_to_column()
