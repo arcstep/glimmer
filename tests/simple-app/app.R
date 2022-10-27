36fbbacd3c9656999f02de56bb66cfe4a62065a9
arcstep <43801@qq.com>
2022-10-27 23:25:24 +0800

    todo: plotly

--- tests/simple-app/app.R
+++ tests/simple-app/app.R
@@ -1,12 +1,19 @@
 library(shiny)
 library(glimmer)
 
+rootPath <- tempdir()
+config_init(rootPath)
+import_init()
+risk_data_init()
+task_queue_init()
+
 ui <- fluidPage(
-  funcEditorUI("func_editor")
+  titlePanel("任务函数"),
+  ui_func_editor("func_editor")
 )
 
 server <- function(input, output, session) {
-  funcEditorServer("func_editor")
+  server_func_editor("func_editor")
 }
 
 shinyApp(ui, server)
--- tests/testthat/test-gali-plotly.R
+++ tests/testthat/test-gali-plotly.R
@@ -1,46 +1,46 @@
 test_that("直方图", {
-  mtcars |> gali_plot_hist(e_x = "mpg") |>
+  mtcars |> plot_hist(e_x = "mpg") |>
     testthat::expect_snapshot()
 })
 
 test_that("散点图", {
-  mtcars |> gali_plot_marker(e_x = "mpg", e_y = "disp", f1_alpha = 0.6) |>
+  mtcars |> plot_marker(e_x = "mpg", e_y = "disp", f1_alpha = 0.6) |>
     testthat::expect_snapshot()
 })
 
 test_that("饼图", {
   mtcars |>
-    gali_plot_pie(e_value = "cyl") |>
+    plot_pie(e_value = "cyl") |>
     testthat::expect_snapshot()
   
   mtcars |> count(cyl) |>
-   gali_plot_pie2(e_value = "n", f1s_pull = c(0.1, 0, 0)) |>
+   plot_pie2(e_value = "n", f1s_pull = c(0.1, 0, 0)) |>
     testthat::expect_snapshot()
 })
 
 test_that("柱图", {
   mtcars |>
     count(cyl) |>
-    gali_plot_bar(e_x = "cyl", e_y = "n") |>
+    plot_bar(e_x = "cyl", e_y = "n") |>
     testthat::expect_snapshot()
 })
 
 test_that("线图", {
   mtcars |>
     count(cyl) |>
-    gali_plot_line(e_x = "cyl", e_y = "n") |>
+    plot_line(e_x = "cyl", e_y = "n") |>
     testthat::expect_snapshot()
 })
 
 test_that("面积图", {
   mtcars |>
     count(cyl) |>
-    gali_plot_area(e_x = "cyl", e_y = "n") |>
+    plot_area(e_x = "cyl", e_y = "n") |>
     testthat::expect_snapshot()
 
   mtcars |> group_by(cyl) |>
     summarise(displ = mean(disp)) |>
-    gali_plot_area(e_x = "cyl", e_y = "displ") |>
+    plot_area(e_x = "cyl", e_y = "displ") |>
     testthat::expect_snapshot()
 })
 
@@ -48,6 +48,6 @@ test_that("极坐标柱图", {
   mtcars |>
     count(cyl) |>
     mutate(cyl = sprintf("CYL: %d", cyl)) |>
-    gali_plot_barpolar(e_theta = "cyl", e_r = "n") |>
+    plot_barpolar(e_theta = "cyl", e_r = "n") |>
     testthat::expect_snapshot()
 })
