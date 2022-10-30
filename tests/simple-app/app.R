e9e7645653e69b65688280e53b484418bda847fc
arcstep <43801@qq.com>
2022-10-30 10:19:36 +0800

    prevent-shiny-autoload: R/_disable_autoload.R

--- tests/simple-app/app.R
+++ tests/simple-app/app.R
@@ -6,16 +6,16 @@ import_init()
 risk_data_init()
 task_queue_init()
 
-task_create(taskId = "demo-task1") |>
+task_create(taskId = "demo-task1", force = TRUE) |>
   task_item_add(type = "func", script = "ds_demo", params = list(demoDataset = "mtcars")) |>
   task_item_add(type = "func", script = "ds_head", params = list(n = 20)) |>
   task_item_add(type = "func", script = "ds_arrange", params = list(columns = "disp", desc = TRUE))
 
-task_create(taskId = "demo-task2") |>
+task_create(taskId = "demo-task2", force = TRUE) |>
   task_item_add(type = "func", script = "ds_demo", params = list(demoDataset = "mpg")) |>
   task_item_add(type = "func", script = "ds_arrange", params = list(columns = "displ", desc = TRUE))
 
-task_create(taskId = "demo-task3") |>
+task_create(taskId = "demo-task3", force = TRUE) |>
   task_item_add(type = "func", script = "ds_demo", params = list(demoDataset = "mpg")) |>
   task_item_add(type = "func", script = "ds_count", params = list(columns = "model", sort = TRUE, name = "车型数量"))
 
