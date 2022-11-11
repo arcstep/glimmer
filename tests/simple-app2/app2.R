9e128dcf1e44814f074374b5dde3834b77dced3a
arcstep <43801@qq.com>
2022-11-11 23:45:52 +0800

    rename: app.R -> app2.R

--- tests/simple-app2/app2.R
+++ tests/simple-app2/app2.R
@@ -6,6 +6,7 @@ library(tidyr)
 library(dplyr)
 library(plotly)
 library(glimmer)
+library(sortable)
 
 glimmer::config_load("./")
 import_init()
@@ -41,21 +42,57 @@ mod_preview_DT_ui <- function(id) {
     tabs = list(
       list(
         menu = "摘要",
-        id = ns("first_tab")),
-      content = plotOutput(ns("plot1")),
+        id = ns("sum-tab"),
+        content = uiOutput(ns("summary")) 
+      ),
       list(
         menu = "明细",
-        id = ns("second_tab"),
-        content = semantic_DTOutput(ns("DT")))
+        id = ns("detail-tab"),
+        content = semantic_DTOutput(ns("DT"))
+      )
     ),
-    active = ns("second_tab"),
-    id = ns("exampletabset")
+    active = ns("detail-tab"),
+    id = ns("DT-tab")
   )
-  
 }
 #
 mod_preview_DT_server <- function(id, data) {
   moduleServer(id, function(input, output, session) {
+    # cards-items-ui
+    items <- names(data) |> purrr::map(function(itemName) {
+      card(
+        div(
+          class = "content",
+          div(
+            class = "header",
+            itemName
+          ),
+          div(
+            class = "meta",
+            class(data[[itemName]])[1]
+          ),
+          div(
+            class = "description",
+            paste(length(unique(data[[itemName]])), "个值")
+          )
+        )
+      )
+    })
+    #
+    output$summary <- renderUI({
+      tagList(
+        div(
+          class = "ui raised segment",
+          div(
+            a(class="ui green ribbon label", "数据框"),
+            br(),
+            p(sprintf("%d行, %d列", nrow(data), ncol(data)))
+          )),
+        cards(
+          class = "two",
+          !!!items
+        ))
+    })
     output$DT <- DT::renderDataTable({
       data |> semantic_DT()
     })
